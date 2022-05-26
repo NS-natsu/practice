e7660f382d7178d88f1143097e23b6c4461eb50f
NS-natsu <104343731+NS-natsu@users.noreply.github.com>
2022-05-26 20:45:40 +0900

    Update test.js

--- test.js
+++ test.js
@@ -2,13 +2,24 @@ let intarvalID;
 
 //処理速度確認用
 function test(){
-  const start = performance.now();
-  // code here...
+	const start = performance.now();
+	// code here...
 	for(let i = 0; i < 100000000; i++);
 	const end = performance.now();
-
 	LineGraph.addData(end - start);
 	LineGraph.draw();
+}
+
+function stopTest(){
+	clearInterval(intarvalID);
+	intarvalID = null;
+	
+	LineGraph.eraseData();
+}
+
+function startTest = (func=test, time=100) => {
+	stopTest();
+	intarvalID = setInterval(func, time);
 }
 
 intarvalID = setInterval(test, 16);
